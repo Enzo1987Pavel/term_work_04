bcd2d406434eaecb06b1ba678d88b1bf36553b60
Enzo1987Pavel <Enzo1987@mail.ru>
2022-07-28 18:31:34 +0300

    Edited for working frontend and Swagger

--- project/server.py
+++ project/server.py
@@ -1,9 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 
 from project.exceptions import BaseServiceError
 from project.setup.api import api
 from project.setup.db import db
+from project.views import auth_ns, genres_ns, user_ns, directors_ns, movies_ns
 
 
 def base_service_error_handler(exception: BaseServiceError):
@@ -11,13 +12,25 @@ def base_service_error_handler(exception: BaseServiceError):
 
 
 def create_app(config_obj):
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="../templates", static_folder="../static")
     app.config.from_object(config_obj)
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
 
     CORS(app=app)
     db.init_app(app)
     api.init_app(app)
 
+    # Регистрация эндпоинтов
+    api.add_namespace(auth_ns)
+    api.add_namespace(user_ns)
+    api.add_namespace(genres_ns)
+    api.add_namespace(directors_ns)
+    api.add_namespace(movies_ns)
+
     app.register_error_handler(BaseServiceError, base_service_error_handler)
 
     return app
+
